5e76afec7e0cc211a2af42f97285ff7910f54ebc
msaki <msaki061@gmail.com>
2024-07-06 22:54:06 +0900

    임시 코드 진행

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,12 @@ module.exports = function(app) {
         createProxyMiddleware({
             target: 'http://localhost:8080',
             changeOrigin: true,
+            onProxyReq: (proxyReq, req, res) => {
+                console.log(`Proxying request: ${req.method} ${req.url}`);
+            },
+            onProxyRes: (proxyRes, req, res) => {
+                console.log(`Received response: ${proxyRes.statusCode} for ${req.method} ${req.url}`);
+            }
         })
     );
 };
