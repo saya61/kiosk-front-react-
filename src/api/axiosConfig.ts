32577390904ad3a254fae9f4542b49638f539f50
saya61 <msaki061@gmail.com>
2024-08-19 15:52:40 +0900

    CI/CD 를 위한 코드 수정

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
+const axiosInstance = axios.create({
+    baseURL: API_URL,  // API URL을 환경 변수로부터 가져옴
+    withCredentials: true,  // 쿠키 전송을 허용
+});
+
 // 요청 인터셉터 설정
-axios.interceptors.request.use(
+axiosInstance.interceptors.request.use(
     config => {
         const token = localStorage.getItem('token');
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
-        config.withCredentials = true; // withCredentials 설정 추가
         return config;
     },
     error => {
@@ -15,4 +21,4 @@ axios.interceptors.request.use(
     }
 );
 
-export default axios;
+export default axiosInstance;
