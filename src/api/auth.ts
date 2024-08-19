4fc99096a212962986fbadeb1602678d8d01a15e
saya61 <msaki061@gmail.com>
2024-08-19 16:58:55 +0900

    Revert "CI/CD 를 위한 코드 수정"
    This reverts commit 32577390904ad3a254fae9f4542b49638f539f50.

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,10 @@
-import axiosInstance from './axiosConfig';
+import axios from './axiosConfig';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const login = async (name: string, password: string) => {
     try {
-        const response = await axiosInstance.post(`/api/kk/kiosk/login`, {
+        const response = await axios.post(`${API_URL}/api/kk/kiosk/login`, {
             name,
             password
         });
@@ -14,7 +16,7 @@ export const login = async (name: string, password: string) => {
 
 export const fetchStoreInfo = async (adminId: number) => {
     try {
-        const response = await axiosInstance.get(`/api/kk/store/${adminId}`);
+        const response = await axios.get(`${API_URL}/api/kk/store/${adminId}`);
         return response.data; // 성공 시 상점 정보 반환
     } catch (error) {
         throw error;
--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,19 +1,13 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-const axiosInstance = axios.create({
-    baseURL: API_URL,  // API URL을 환경 변수로부터 가져옴
-    withCredentials: true,  // 쿠키 전송을 허용
-});
-
 // 요청 인터셉터 설정
-axiosInstance.interceptors.request.use(
+axios.interceptors.request.use(
     config => {
         const token = localStorage.getItem('token');
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
+        config.withCredentials = true; // withCredentials 설정 추가
         return config;
     },
     error => {
@@ -21,4 +15,4 @@ axiosInstance.interceptors.request.use(
     }
 );
 
-export default axiosInstance;
+export default axios;
